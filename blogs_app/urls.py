b75698e4981a98a5f30e0fdc5b7847e682c881a7
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-30 10:11:20 -0600

    add new view

--- blogs_app/urls.py
+++ blogs_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogs', views.index),
+    path('', views.index),
+    path('new/', views.new)
 ]
--- blogs_app/views.py
+++ blogs_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    index_placeholder = "placeholder to later display a list of all blogs"
-    print(index_placeholder)
-    return index_placeholder
+    return render(request, 'index.html')
+
+def new(request):
+    return render(request, 'new.html')
