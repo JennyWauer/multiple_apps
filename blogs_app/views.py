37ae00e09e00e0419f3752a153b10156b8dd10fe
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-30 08:34:00 -0600

    add index view to users_app

---
+++ blogs_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    index_placeholder = "placeholder to later display a list of all blogs"
+    print(index_placeholder)
+    return index_placeholder
---
+++ surveys_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    index_placeholder = "placeholder to later display a list of all surveys"
+    print(index_placeholder)
+    return index_placeholder
