021d1f2ba1308aca9648d251894b4e003f44609a
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-30 10:30:26 -0600

    add survey method

--- surveys_app/urls.py
+++ surveys_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('surveys/', views.index),
+    path('', views.index),
 ]
--- surveys_app/views.py
+++ surveys_app/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    index_placeholder = "placeholder to later display a list of all surveys"
-    print(index_placeholder)
-    return index_placeholder
+    return render(request, 'index.html')
